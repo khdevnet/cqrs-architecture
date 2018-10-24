9f74664cedc69e72fc5a6cd4fdf39b3e8abddcc0
anton.shcherbyna <anton.shcherbyna@sigma.software>
2018-10-24 16:02:27 +0300

    Rename Msessage subscriber

--- SW.Store.Core/Messages/IMessageSubscriberFactory.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SW.Store.Core.Messages
-{
-    //public interface IMessageSubscriberFactory
-    //{
-    //    IQueueSubscriber<TMessage> Create<TMessage>(string hostName, string queueName, string routingKey) where TMessage : IMessage; 
-    //}
-}
